952f12c00724639e34731e7c581774607d7dd3e0
Piotr Binkowski <piotr.binkowski1@gmail.com>
2018-12-26 13:32:01 +0100

    sbc030: add bitbang spi support

--- board/pbinkowski/sbc030/sbc030.c
+++ board/pbinkowski/sbc030/sbc030.c
@@ -7,10 +7,17 @@
  * TsiChung Liew (Tsi-Chung.Liew@freescale.com)
  */
 
+#include <asm/io.h>
 #include <config.h>
 #include <common.h>
+#include <spi.h>
 
 DECLARE_GLOBAL_DATA_PTR;
+
+#define PIT_BASE 0x20000000
+#define PCDDR 9
+#define PCDR 25
+
 
 int checkboard(void)
 {
@@ -25,6 +32,74 @@ int dram_init(void)
 
 	return 0;
 };
+
+/*
+ * PC7 - MOSI
+ * PC6 - MISO
+ * PC4 - CS
+ * PC2 - SCK
+ */
+
+#define PC7 (1<<7)
+#define PC6 (1<<6)
+#define PC4 (1<<4)
+#define PC2 (1<<2)
+
+int board_late_init(void)
+{
+	unsigned char dir = 0;
+	dir |= PC7 | PC4 | PC2;
+	writeb(dir, PIT_BASE + PCDDR);
+	return 0;
+}
+
+static void set_pin(unsigned char pin, unsigned char set)
+{
+	unsigned char reg;
+	reg = readb(PIT_BASE + PCDR);
+
+	if(set)
+		reg |= pin;
+	else
+		reg &= ~pin;
+
+	writeb(reg, PIT_BASE + PCDR);
+
+}
+
+#ifdef CONFIG_CMD_SPI
+
+int spi_cs_is_valid(unsigned int bus, unsigned int cs)
+{
+	return 1;
+}
+
+void spi_cs_activate(struct spi_slave *slave)
+{
+	set_pin(PC4, 0);
+}
+
+void spi_cs_deactivate(struct spi_slave *slave)
+{
+	set_pin(PC4, 1);
+}
+
+void sbc030_spi_sda(int set)
+{
+	set_pin(PC7, set);
+}
+
+void sbc030_spi_scl(int set)
+{
+	set_pin(PC2, set);
+}
+
+unsigned char sbc030_spi_read(void)
+{
+	return !!(readb(PIT_BASE + PCDR) & PC6);
+}
+
+#endif /* CONFIG_CMD_SPI */
 
 #ifdef CONFIG_FLASH_CFI_LEGACY
 ulong board_flash_get_legacy(ulong base, int banknum, flash_info_t *info)
